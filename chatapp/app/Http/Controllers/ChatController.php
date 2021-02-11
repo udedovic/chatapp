35f8e58c5a65bcfa7e5d4cf401705a52afaf8d81
u.dedovic <u.dedovic@gmail.com>
2021-02-11 22:17:01 +0100

    dodata mogucnost komuniciranja izmedju vise ljudi

--- chatapp/app/Http/Controllers/ChatController.php
+++ chatapp/app/Http/Controllers/ChatController.php
@@ -6,21 +6,23 @@ use Illuminate\Http\Request;
 use App\Models\ChatRoom;
 use App\Models\ChatMessage;
 use Illuminate\Support\Facades\Auth;
+use App\Events\NewChatMessage;
 
 class ChatController extends Controller
 {
-    public function rooms (Request $request){
+    public function rooms(Request $request)
+    {
 
         return ChatRoom::all();
-
     }
-    public function messages (Request $request, $roomId){
+    public function messages(Request $request, $roomId)
+    {
 
-        return ChatMessage::where('chat_room_id',$roomId)->with('user')->orderBy('created_at','DESC')->get();
-
+        return ChatMessage::where('chat_room_id', $roomId)->with('user')->orderBy('created_at', 'DESC')->get();
     }
 
-    public function newMessage (Request $request, $roomId){
+    public function newMessage(Request $request, $roomId)
+    {
 
         $newMessage = new ChatMessage();
         $newMessage->user_id = Auth::id();
@@ -28,8 +30,8 @@ class ChatController extends Controller
         $newMessage->message = $request->message;
         $newMessage->save();
 
+        broadcast(new NewChatMessage($newMessage))->toOthers();
+
         return $newMessage;
-
     }
-
 }
